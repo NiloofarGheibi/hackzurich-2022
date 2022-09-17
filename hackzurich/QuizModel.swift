8e410f251037608c683ea91748e16820afc27232
Rocha <irocha@amazon.com>
2022-09-17 02:16:26 +0200

    Adding multi page view.

---
+++ hackzurich/QuizModel.swift
@@ -0,0 +1,30 @@
+//
+//  QuizModel.swift
+//  ShoppingAppUI
+//
+//  Created by Isabelly Rocha
+//
+import Foundation
+import SwiftUI
+
+struct Quiz {
+    var currentQuestionIndex: Int
+    var quizModel: QuizModel
+    var quizCompleted: Bool = false
+    var quizWinningStatus: Bool = false
+}
+
+struct QuizModel {
+    var question: String
+    var answer: String
+    var optionsList: [QuizOption]
+}
+
+struct QuizOption : Identifiable {
+    var id: Int
+    var optionId: String
+    var option: String
+    var color: Color
+    var isSelected : Bool = false
+    var isMatched : Bool = false
+}
